0e0e35171a5632063e50cff757f4a191dacdbc12
meiyuouo <meiyu19980928@gmail.com>
2024-06-03 20:19:45 +0800

    update basic

--- 00_hello_world/app.py
+++ 00_hello_world/app.py
@@ -3,6 +3,21 @@ def hello():
     return {"message": "Hello, world!"}
 
 
+def demo_print():
+    print("I say 'Hello'")
+    print('I say "Hello"')
+    print('I say \n\'Hello\'')
+
+
+def demo_input():
+    answer = input("say something: ")
+    print(answer)
+
+
 if __name__ == "__main__":
     result = hello()
     print(result)
+
+    demo_print()
+
+    demo_input()
