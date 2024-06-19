ec75d93e9a7319c00f889d316289fe11c468b191
meiyuouo <meiyu19980928@gmail.com>
2024-06-19 19:08:11 +0800

    fix tests

--- 02_pytest/tests/assert/test_assert.py
+++ 02_pytest/tests/assert/test_assert.py
@@ -1,12 +1,12 @@
 import pytest
 
 
-def test_assertEqual_string(self):
+def test_assertEqual_string():
     assert "foo".upper() == "FOO"
     assert "foo" == "foo"
 
 
-def test_assertEqual_array(self):
+def test_assertEqual_array():
     string_array = "hello world".split()
     expect_array = ["hello", "world"]
 
--- 02_pytest/tests/assert/test_assert_raise.py
+++ 02_pytest/tests/assert/test_assert_raise.py
@@ -1,18 +1,18 @@
 import pytest
 
 
-def test_assertRaises_TypeError(self):
+def test_assertRaises_TypeError():
     with pytest.raises(TypeError):
         "hello world".split(2)
 
 
-def test_assertRaises_regex(self):
+def test_assertRaises_regex():
     with pytest.raises(TypeError, match=r"^must be str or None, not int$"):
         "hello world".split(2)
 
 
 # ExceptionInfo instance's main attributes are type, value, traceback
-def test_assertRaises_exceptionInfo(self):
+def test_assertRaises_exceptionInfo():
     with pytest.raises(TypeError) as exceptionInfo:
         "hello world".split(2)
 
